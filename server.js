855304d6fc158189a7db1ccc5e9469ed4a5239e3
Rishal <Rishal.mhd009@gmail.com>
2023-08-01 18:38:14 +0530

    task one done

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-const app = require('express')
-
+const app = require("express");
 
 const PORT = 3000; // We should put port number in env file in projects
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+});

c3b41973f4efe79593cdc95fcfc1451399b0c1c5
Rishal <Rishal.mhd009@gmail.com>
2023-08-01 19:19:56 +0530

    task 2 done

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require("express");
+const fs = require("fs");
 
-const app = express()
+const app = express();
 
 const PORT = 3000; // We should put port number in env file in projects
-
 
 // task 1 , print hello world
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-//
+//task 2, sum of integers in array
+const sumOfArray = (arr) => {
+  return arr.reduce((sum, num) => sum + num, 0);
+};
+
+// testing of task 2
+console.log(sumOfArray([1, 2, 3, 4])); // consoles 10
+
+//task 3, file reading
+const countWords = (text) => {
+    
+}
+
+const fileName = "data.txt"; //assuming the file is in the directory
+
 
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);
